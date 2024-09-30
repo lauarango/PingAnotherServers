0f0ee91cbd1e2ed435778e082dd9056d67e0968c
lauarango <88212130+lauarango@users.noreply.github.com>
2024-09-30 00:55:43 -0500

    Update script.sh

--- script.sh
+++ script.sh
@@ -3,10 +3,10 @@
 host_ips="host_ip_list"
 for ip in $(cat host_ip_list)
 do
-  ping -c1 $host_ips &> /dev/null
+  ping -c1 $ip &> /dev/null
   if [ $? -eq 0 ]; then
-  echo "$host_ips Good Network Connectivity"
+  echo "$ip Good Network Connectivity"
   else
-  echo "$host_ips Bad Network Connectivity"
+  echo "$ip Bad Network Connectivity"
   fi
   
